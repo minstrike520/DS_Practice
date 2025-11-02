26769f519197b08a5b9774506fc3d312558da67e
minstrike520 <minstrike520@gmail.com>
2025-11-02 23:14:10 +0800

    trivial fixes

--- hw1/main.c
+++ hw1/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #define PLN_ALLOC_SIZE 100
 
--- queue/array-queue.c
+++ queue/array-queue.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #define STACK_MAX_LENGTH 10000
 
